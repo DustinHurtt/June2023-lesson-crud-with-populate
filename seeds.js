b677c2e47c59eb1772f7e2954df0f27ed7786b36
Dustin Hurtt <dustin_hurtt@outlook.com>
2023-07-14 17:34:44 -0400

    lesson done

---
+++ seeds.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const User = require('./models/User.model');
+
+// ℹ️ Connects to the database
+mongoose
+  .connect('mongodb://127.0.0.1:27017/lesson-crud-with-populate')
+  .then(x => {
+    console.log(`Connected to Mongo database: "${x.connections[0].name}"`);
+  })
+  .catch(err => {
+    console.log(`An error occurred while connecting to the Database: ${err}`);
+  });
+
+// User.collection.drop();
+
+const fakeUsers = [
+  {
+    username: 'amartin07'
+  },
+  {
+    username: 'luca85'
+  },
+  {
+    username: 'madmax'
+  }
+];
+
+User.create(fakeUsers)
+  .then(dbUsers => {
+    console.log(`Created ${dbUsers.length} users`);
+    mongoose.connection.close();
+  })
+  .catch(err => console.log(`An error occurred while creating fake users in the DB: ${err}`));
